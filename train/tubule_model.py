cfb0b9439808ab9bf00a3abea6300da0f4348491
MDyakova <m.dyakova.ml@gmail.com>
2024-12-06 21:12:39 +0200

    add load images script

--- train/tubule_model.py
+++ train/tubule_model.py
@@ -13,14 +13,14 @@ from tifffile import TiffFile
 import cv2
 import os
 from imantics import Polygons, Mask
-from tqdm import tqdm_notebook
 import json
 
 import zipfile
 from ultralytics import YOLO
 import shutil
 from matplotlib.patches import Polygon
-from utilities import make_dataset_directory
+from utilities import (make_dataset_directory,
+                       load_images)
 import time
 
 # Load config
@@ -32,11 +32,35 @@ with open(os.path.join(os.path.join('files', 'user_config.json')), 'r', encoding
 nori_images = config['data_information']['nori_images']
 protein_layer = config['data_information']['protein_layer']
 lipid_layer = config['data_information']['lipid_layer']
+tubule_masks_layer = config['data_information']['tubule_masks_layer']
 
 # Model's info
 model_name = config['model_information']['model_name']
+modifications = config['model_information']['modifications']
+crop_size = config['model_information']['crop_size']
 
 # Create train directory
 make_dataset_directory(os.path.join('files', 'train'), model_name)
+
+# Split to train and validation subsets
+all_files = os.listdir(nori_images)
+test_groups = all_files[int(len(all_files)*0.8):]
+
+train_images = list(filter(lambda p: (p not in test_groups), all_files))
+val_images = list(filter(lambda p: (p in test_groups), all_files))
+
+# save train subset
+for file_name in train_images[0:]:
+    file_name_save = file_name.split('.')[0]
+    directory = 'train'
+
+    (all_images,
+     all_masks,
+     all_images_names) = load_images(nori_images,
+                                    file_name,
+                                    protein_layer,
+                                    lipid_layer,
+                                    tubule_masks_layer,
+                                    crop_size=crop_size)
 
 time.sleep(1000)
--- train/utilities.py
+++ train/utilities.py
@@ -32,3 +32,63 @@ def make_dataset_directory(dataset_folder, dataset_name):
         for set_folder in ['train', 'val']:
             os.makedirs(os.path.join(os.path.join(dataset_folder, dataset_name),
                                      folder, set_folder), exist_ok=True)
+
+# polygons for test results
+def get_polygons_predict(mask_label):
+    """
+    Convert masks to polygons
+    """
+    polygons = Mask((mask_label>0)).polygons()
+    polygons = polygons.points
+    return polygons
+
+def load_images(nori_images,
+                file_name,
+                protein_layer,
+                lipid_layer,
+                tubule_masks_layer,
+                crop_size=640):
+    """
+    Load and preprocessing big tiff images and masks to tiles
+    """
+    with TiffFile(os.path.join(nori_images, file_name)) as tif:
+      image = tif.asarray()
+
+      # Extract masks
+      mask = image[tubule_masks_layer]
+
+      # Extract nori_data
+      protein = image[protein_layer, :, :]
+      lipid = image[lipid_layer, :, :]
+      # While is empty
+      water = image[lipid_layer, :, :]*0
+      image = np.stack([protein, lipid, water], axis=0)
+
+    height, width = image[0].shape
+    all_images = []
+    all_masks = []
+    all_images_names = []
+
+    # Make crops with 50% overlap
+    for i in range(0, height - crop_size, crop_size//2):
+        for j in range(0, width - crop_size, crop_size//2):
+            image_crop = image[:, i:i+crop_size, j:j+crop_size]
+            mask_crop = mask[i:i+crop_size, j:j+crop_size]
+            all_layers = []
+            # Filter extremal higher values
+            for layer in range(0, 3):
+                image_layer = image_crop[layer]
+                percentile_99 = np.percentile(image_layer, 99)
+                image_layer = np.where((image_layer>percentile_99), percentile_99, image_layer)
+                image_layer = image_layer/image_layer.max()
+                if layer==2:
+                    image_layer = image_layer*0
+
+                all_layers.append(image_layer)
+            image_crop = np.array(all_layers)
+            all_images.append(image_crop)
+            crop_name = '_'.join([file_name.split('.')[0], str(i), str(j)])
+            all_images_names.append(crop_name)
+            all_masks.append(mask_crop)
+
+    return all_images, all_masks, all_images_names
