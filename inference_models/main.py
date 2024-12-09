d67986980f2afaff2fa9db25a3d0d7b5fd67c862
MDyakova <m.dyakova.ml@gmail.com>
2024-12-09 10:18:28 +0200

    add model results

--- inference_models/main.py
+++ inference_models/main.py
@@ -11,6 +11,7 @@ from PIL import Image, ImageDraw
 import tifffile
 from tifffile import TiffFile
 import cv2
+import json
 import os
 from imantics import Polygons, Mask
 from tqdm import tqdm_notebook
@@ -33,7 +34,9 @@ import albumentations as A
 from albumentations.pytorch import ToTensorV2
 from torchvision import models
 
-from utilities import make_output_directory
+from utilities import (make_output_directory,
+                       image_filter,
+                       image_to_unet)
 from train_models.model_utilities import UNet
 
 import time
@@ -60,17 +63,83 @@ if __name__ == "__main__":
     # Model's info
     tubule_model_path = config['models']['tubule_model']
     nuclei_model_path = config['models']['nuclei_model']
+    crop_size = config['models']['crop_size']
 
     # Create output directory
     make_output_directory(output_folder)
+    images_save = os.path.join(output_folder, 'images')
+    labels_save = os.path.join(output_folder, 'labels')
+    tiff_save = os.path.join(output_folder, 'tiff_files')
 
     # Load YOLO model
     tubule_model = YOLO(tubule_model_path)
 
     # Load Unet model
-    model_nuclei = UNet().to(device)
-    model_nuclei.load_state_dict(torch.load(nuclei_model_path,
+    nuclei_model = UNet().to(device)
+    nuclei_model.load_state_dict(torch.load(nuclei_model_path,
                                             map_location=device))
-    model_nuclei.eval()
+    nuclei_model.eval()
+
+    # All test samples
+    image_names = os.listdir(nori_images)
+    # Existing results
+    saved_labels = os.listdir(labels_save)
+
+    for file_name in image_names:
+        file_name_save = file_name.split('.')[0]
+        labels_name = file_name_save + '.csv'
+        if ('.tif' in file_name) & (labels_name not in saved_labels):
+            # Load image
+            image_path = os.path.join(nori_images, file_name)
+            with TiffFile(image_path) as tif:
+                image = tif.asarray()
+            # Filter outliers
+            image_full = image_filter(image)
+            image_full = Image.fromarray((image_full*255).transpose((1, 2, 0)).astype(np.uint8))
+
+            height, width = image[0].shape
+            # Make empty masks for all segmented objects
+            mask_for_tubules = np.zeros((height, width, 4), dtype=int)
+            mask__for_nuclei = np.zeros((height, width), dtype=int)
+            mask__for_lumen = np.zeros((height, width), dtype=int)
+            mask__for_prob = np.zeros((height, width), dtype=int)
+            step = 0
+            mask_n = 1
+            mask_nucl_id = 1
+            mask_lumen_id = 1
+            nucleus_polygons = []
+            lumen_polygons = []
+
+            # Make tiles with 50% interchange
+            for i in range(0, height - crop_size//2, crop_size//2):
+                for j in range(0, width - crop_size//2, crop_size//2):
+                    # Determine mask layer to save polygons for crop
+                    if (i%crop_size==0) & (j%crop_size==0):
+                        layer = 0
+                    elif (i%crop_size==0) & (j%crop_size!=0):
+                        layer = 1
+                    elif (i%crop_size!=0) & (j%crop_size==0):
+                        layer = 2
+                    else:
+                        layer = 3
+
+                    # Make crop and filter them
+                    image_crop = image[:, i:i+crop_size, j:j+crop_size]
+                    image_crop = image_filter(image, image_crop, is_crop=True)
+                    image_crop = (image_crop * 255).astype(np.uint8)
+                    height_crop, width_crop, _ = image_crop.shape
+
+                    # Get tubule predictions
+                    tubule_results = tubule_model(image_crop)
+
+                    # Get nuclei predictions
+                    image_for_unet = image_to_unet(image_crop).to(device)
+                    nuclei_results = nuclei_model(image_for_unet)
+                    nuclei_results = (nuclei_results > 0.9).float()  # Binarize prediction
+                    nuclei_results = nuclei_results.squeeze().cpu().numpy()
+
+                    nuclei_results = np.array(Image.fromarray(nuclei_results.astype(np.uint8)*255)
+                                              .resize((height_crop, width_crop)))
+
 
 
--- inference_models/utilities.py
+++ inference_models/utilities.py
@@ -43,3 +43,59 @@ def make_output_directory(output_folder):
     folders = ['images', 'labels', 'tiff_files']
     for folder in folders:
         os.makedirs(os.path.join(output_folder, folder), exist_ok=True)
+
+def image_filter(image, image_crop=None, is_crop=False):
+    """
+    Function remove outliers
+    """
+    all_layers = []
+    if is_crop:
+        for layer in range(0, 3):
+            image_layer = image_crop[layer]
+            image_layer_all = image[layer]
+            all_percentile = []
+            percentile_99 = np.percentile(image_layer, 99)
+            image_layer = np.where((image_layer>percentile_99), percentile_99, image_layer)
+            # Normalize each layer
+            image_layer = image_layer/image_layer.max()
+            # Keep water layer is 0
+            if layer==2:
+                image_layer = image_layer*0
+            all_layers.append(image_layer)
+    else:
+        for layer in range(0, 3):
+            image_layer = image[layer]
+            all_percentile = []
+            # Compute percentile 99 for tiles to remove big extrime areas
+            for step_i in range(image_layer.shape[0]//256):
+                for step_j in range(image_layer.shape[1]//256):
+                    image_layer_crop = image_layer[step_i*256:(step_i+1)*256, step_j*256:(step_j+1)*256]
+                    percentile_99 = np.percentile(image_layer_crop, 99)
+                    all_percentile.append(percentile_99)
+            percentile_99 = np.median(all_percentile)
+            image_layer = np.where((image_layer>percentile_99), percentile_99, image_layer)
+            # Normalize each layer
+            image_layer = image_layer/image_layer.max()
+            # Keep water layer is 0
+            if layer==2:
+                image_layer = image_layer*0
+            all_layers.append(image_layer)
+    filtered_image = np.array(all_layers)
+    return filtered_image
+
+#
+def image_to_unet(image_crop, crop_size):
+    """
+    Function to load a single image for unet model
+    """
+    # Define the transformation to match the model input
+    transform = transforms.Compose([
+        transforms.Resize((crop_size, crop_size)),  # Resize to match your input size
+        transforms.ToTensor(),
+        transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
+    ])
+    # Convert image to unet format
+    img = Image.fromarray(image_crop.astype('uint8')).convert("RGB")
+    # img = Image.fromarray(image).convert("RGB")
+    img = transform(img)
+    return img.unsqueeze(0)  # Add batch dimension
